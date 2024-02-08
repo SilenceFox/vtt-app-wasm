639e2289347d584be0853345af19f3231c9a4c87
SilenceFox <cristianplaybr@gmail.com>
2024-02-08 04:43:43 -0300

    Did the basic layout
    CSS with sir is pretty good! But code feels less reusable, i will need to port it to a proper style.css
    It literally does nothing for now

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,5 @@
 #![allow(non_snake_case)]
 
-use dioxus_router::prelude::*;
-
 use dioxus::prelude::*;
 use log::LevelFilter;
 use sir::{AppStyle, css, global_css};
@@ -18,7 +16,7 @@ fn main() {
 fn app(cx: Scope) -> Element {
     global_css!("
         body {
-            background: linear-gradient(45deg, #80AAFF, #F0F0F0);
+            background: linear-gradient(180deg, #000044, #000000);
             height: 100vh;
             width: 100vw;
             display: flex;
@@ -29,22 +27,14 @@ fn app(cx: Scope) -> Element {
     ");
     render! {
         AppStyle {}
-        Router::<Route> {}
+        Home {}
     }
 }
 
-#[derive(Clone, Routable, Debug, PartialEq)]
-enum Route {
-    #[route("/")]
-    Home {},
-    #[route("/blog/:id")]
-    Blog { id: i32 },
-}
 
 #[component]
 fn Blog(cx: Scope, id: i32) -> Element {
     render! {
-        Link { to: Route::Home {}, "Go to counter" }
         "Blog post {id}"
     }
 }
@@ -52,47 +42,117 @@ fn Blog(cx: Scope, id: i32) -> Element {
 #[component]
 fn Home(cx: Scope) -> Element {
     let button = css!("
+            color: darkslategray;
             border: none;
             padding: 4px 16px;
-            border-radius: 12px 12px 0px 0px;
+            border-radius: 0px 0px 12px 12px;
             width: 25%;
+            height: 2em;
             font-size: 20px;
             box-shadow: 0 0 10px rgba(0, 0, 0, 1);
             
             background: deepskyblue;
             transition: background 0.2s ease-out;
+            transition: color 0.2s ease-out;
+            transition: width 0.5s ease;
 
             &:hover {
+                width: 30%;
+                color: black;
                 background: cyan;
                 cursor: pointer;
             }
     ");
 
     let container2 = css!("
-        background: linear-gradient(45deg, #0000FF, #F0F0F0);
+        background: darkslateblue;
         display: flex;
         width: 100vw;
         gap: 10px;
         padding: 0 10px;
     ");
 
-     let container = css!(
-            "
-            display: flex;
-            flex-direction: deck;
-            align-items: center;
-            gap: 10px;
-            "
+    let wip = css!("
+        color: black;
+        border: none;
+        padding: 4px 16px;
+        border-radius: 0px 0px 12px 12px;
+        width: 25%;
+        font-size: 20px;
+        box-shadow: 0 0 10px rgba(0, 0, 0, 1);
+        background: orange;
+        transition: background 0.2s ease-out;
+        &:hover {
+            background: red;
+            cursor: not-allowed;
+            color: white;
+        }"
     );
 
-    cx.render(rsx! {
-        div {
-            class: "{container2}",
-            button { class: button, "Lucas 1" }
-            button { class: button, "Lucas 2" }
-            button { class: button, "Lucas 3" }
-            button { class: button, "Lucas 4" }
-        }	
 
-    })
+    render! {
+    
+        nav {
+            class: "{container2}",
+            button { class: button, "Home" }
+            button { class: button, "Sound" }
+            button { class: wip, "wip" }
+            button { class: wip, "wip" }
+        }
+        Overview {}
+
+    }
+}
+fn Overview(cx: Scope) -> Element {
+     let container = css!(
+        "
+        display: flex;
+        flex-direction: column;
+        align-items: start;
+        gap: 10px;
+        "
+    );
+    let button = css!("
+        background: #202088;
+        color: white;
+        border: none;
+        padding: 4px 16px;
+        border-radius: 0px 12px 12px 0px; 
+        width: 100%;
+        height: 2.5em;
+        font-size: 20px;
+        box-shadow: 0 0 10px rgba(0, 0, 0, 1);
+        transition: background 0.5s ease;
+        transition: height 0.5s ease-out;
+        &:hover {
+            background: #AA0055;
+            height: 3em;
+        }
+        &:clicked {  //
+            background: #0000FF;
+            height: 2.8em;
+        }
+        " //TODO: Gotta learn this, mainly how Dioxus handles events like toggling a clicked state.
+    );
+        
+    render! {
+        div {
+            display: "flex",
+            align_items: "left",
+            div {
+                class: "{container}",
+                id: "bar",
+                align_self:"center",
+                width: "25%",
+                height: "100%",
+                transition: "height 0.5s ease-out",
+                button { class: button, "Overview", },
+                button { class: button, "Scene", },
+                button { class: button, "Sheet", },
+                button { class: button, "Skills", },
+                button { class: button, "Notes", },
+                div { id:"chat",}
+            }
+        }
+    }
 }
