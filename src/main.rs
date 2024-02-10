7fdb0cdf72bd67b5c29cb6f3044eb38eb96f4d33
SilenceFox <cristianplaybr@gmail.com>
2024-02-09 22:27:42 -0300

    Added Input for managing notifications.

--- src/main.rs
+++ src/main.rs
@@ -13,7 +13,7 @@ fn main() {
 }
 
 fn app(cx: Scope) -> Element {
-    render! {  Home {}  }
+    render! { Home {} }
 }
 
 #[component]
@@ -26,12 +26,9 @@ fn Home(cx: Scope) -> Element {
                 button { class: "wip", "wip" }
                 button { class: "wip", "wip" }
             }
-            div {id: "main",
+            div { id: "main",
                 div { z_index: "50", id: "sidepanel", Sidebar {} }
-                div {
-                    id: "main-window",
-                    Overview {}
-                }
+                div { id: "main-window", Overview {} }
                 NotificationBar {}
             }
         }
@@ -40,7 +37,7 @@ fn Home(cx: Scope) -> Element {
 #[component]
 fn Sidebar(cx: Scope) -> Element {
     render! {
-        div { class: "sidebar", id: "bar", 
+        div { class: "sidebar", id: "bar",
             // Call on Overview when on Overview button focus
             button { class: "side_button", "Overview" }
             button { class: "side_button", "Scene" }
@@ -68,29 +65,51 @@ fn Overview(cx: Scope) -> Element {
     }
 } 
 
+struct NotificationData {
+    title: String,
+    message: String,
+}
 #[component]
 fn NotificationBar(cx: Scope) -> Element {
+    let title = use_state(cx, || "".to_string());
+    let message = use_state(cx, || "".to_string());
+
     render!(
-        div {
-            id: "notifications",
-            ol { width: "100%",
-                id: "notification-container",
-                li{
-                    margin: "5px 0",
-                    id: "notification-child",
-                    "jaguara"
-                },
-                li{
-                    margin: "5px 0",
-                    id: "notification-child",
-                    "jaguara"
-                },
-                li{
-                    margin: "5px 0",
-                    id: "notification-child",
-                    "jaguara"
-                },
+        div { id: "notifications",
+            ol { width: "100%", id: "notification-container",
+                li { margin: "5px 0", id: "notification-child", "jaguara" },
+                NotificationContainer {},
+                form { 
+                    input {
+                        oninput: move |event| title.set(event.value.clone()),
+                        placeholder: "Title"
+                    }
+                }
+                form { 
+                    input{ oninput: move |messag| message.set(messag.value.clone()),
+                        placeholder: "Message"  
+                    }}
+                li { "{title}", br{} "{message}" }
             }
+        }
+    )
+}
+
+fn NotificationContainer(cx: Scope) -> Element {
+    //dinamycally increase the number of notificationsChild inside the container
+    let message_data = NotificationData {
+        title: "Test".to_string(),
+        message: "Very much Tested".to_string(),
+    };
+    render!( NotificationChild(cx, message_data) {}
+)
+}
+
+fn NotificationChild(cx: Scope, message: NotificationData) -> Element {
+    render!(
+        li { class: "notification-child", id: "id",
+            div { class: "notification-child", id: "title", "{message.title}" }
+            div { class: "notification-child", id: "content", "{message.message}" }
         }
     )
 }
